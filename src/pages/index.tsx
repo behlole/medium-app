11245ee07e44e1c70af461a87bf1cf9b869b0925
behlole <behloleaqil@gmail.com>
2023-04-04 05:18:39 +0500

    improved styling on main index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,17 +41,17 @@ export default function Home({posts}: Props) {
             <div className={"grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 pd-2 md:p-6"}>
                 {posts.map((post) => (
                     <Link key={post._id} href={`/post/${post.slug.current}`}>
-                        <div className={"border rounded-lg group cursor-pointer"}>
+                        <div className={"border rounded-lg group cursor-pointer overflow-hidden"}>
                             <img className={
                                 "h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
                             }
                                  src={urlFor(post?.mainImage)?.url()!}/>
                             <div className={"flex justify-between p-5 bg-white"}>
                                 <div>
-                                    <p>
+                                    <p className={"text-lg text-bold"}>
                                         {post.title}
                                     </p>
-                                    <p>
+                                    <p className={"text-xs"}>
                                         {post.description} by {post.author.name}
                                     </p>
                                 </div>
@@ -67,7 +67,8 @@ export default function Home({posts}: Props) {
     )
 }
 export const getServerSideProps = async () => {
-    const query = `*[_type == 'post']{
+    const query = `
+    *[_type == 'post']{
       _id,
         title,
         description,
@@ -77,7 +78,8 @@ export const getServerSideProps = async () => {
           name,
           image
         }
-    }`
+    }
+    `
     const posts = await sanityClient.fetch(query);
     return {
         props: {
