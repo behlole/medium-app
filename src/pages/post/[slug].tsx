d9e676b351ac7b1fef328c5610e9046501615756
behlole <behloleaqil@gmail.com>
2023-04-15 21:57:57 -0400

    implemented react portable text of post body

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,56 +1,82 @@
-import React from "react";
-import {sanityClient, urlFor} from "../../../sanity";
-import Header from "../../../components/Header";
-import {Post} from "../../../typings/typings";
-import {GetStaticProps} from "next";
+import React from 'react'
+import {sanityClient, urlFor} from '../../../sanity'
+import Header from '../../../components/Header'
+import {Post} from '../../../typings/typings'
+import {GetStaticProps} from 'next'
+import PortableText from 'react-portable-text'
 
 interface Props {
-    post: Post
+  post: Post
 }
 
 function Posts({post}: Props) {
-    return <main>
-        <Header/>
-        <img
-            className={"w-full h-40 object-cover"}
-            src={urlFor(post?.mainImage).url()!}
-            alt={"Banner"}
+  return <main>
+    <Header />
+    <img
+      className={'w-full h-40 object-cover'}
+      src={urlFor(post?.mainImage).url()!}
+      alt={'Banner'}
+    />
+    <article className={'max-w-3xl mx-auto p-5'}>
+      <h1 className={'text-3xl mt-10 mb-3'}>{post.title}</h1>
+      <h2 className={'text-xl font-light text-grey-500 mb-2'}>{post.description}</h2>
+      <div className={'flex items-center space-x-2'}>
+        <img className={'h-10 w-10 rounded-full'} src={urlFor(post.author.image).url()} alt={''} />
+        <p className={'font-extralight text-sm'}>Blog Post By <span
+          className={'text-green-600'}>{post.author.name}</span> - Published
+          at {new Date(post._createdAt).toLocaleString()}</p>
+      </div>
+
+      <div className={"mt-10"}>
+        <PortableText
+          dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+          projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+          content={post.body}
+          serializers={{
+            h1: (props: any) => (
+              <h1 className={'text-2xl font-bold my-5'} {...props} />
+            ),
+            h2: (props: any) => (
+              <h1 className={'text-xl font-bold my-5'} {...props} />
+            ),
+            li:({children}:any)=>(
+              <li className={"ml-4 list-disc"}>{children}</li>
+            ),
+            link:({href,children}:any)=>(
+              <a href={href} className={"text-blue-500 hover:underline"}>
+                {children}
+              </a>
+            )
+          }}
         />
-        <article className={"max-w-3xl mx-auto p-5"}>
-            <h1 className={"text-3xl mt-10 mb-3"}>{post.title}</h1>
-            <h2 className={"text-xl font-light text-grey-500 mb-2"}>{post.description}</h2>
-            <div className={"flex items-center space-x-2"}>
-                <img className={"h-10 w-10 rounded-full"} src={urlFor(post.author.image).url()} alt={""}/>
-                <p className={"font-extralight text-sm"}>Blog Post By <span className={"text-green-600"}>{post.author.name}</span> - Published
-                    at {new Date(post._createdAt).toLocaleString()}</p>
-            </div>
-        </article>
-    </main>
+      </div>
+    </article>
+  </main>
 }
 
 export default Posts
 
 export const getStaticPaths = async () => {
-    const posts = await sanityClient.fetch(`
+  const posts = await sanityClient.fetch(`
     *[_type=='post']{
         _id,
         slug{
             current
         }
     }`
-    );
-    const paths = posts.map((post: Post) =>
-        ({
-            params: {
-                slug: post.slug.current
-            }
-        })
-    )
-    return {paths, fallback: 'blocking'}
+  )
+  const paths = posts.map((post: Post) =>
+    ({
+      params: {
+        slug: post.slug.current
+      }
+    })
+  )
+  return {paths, fallback: 'blocking'}
 }
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
-    const query = `
+  const query = `
     *[_type=="post" && slug.current==$slug]{
         _id,
         title,
@@ -70,17 +96,17 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
         body
     }
     `
-    let post = await sanityClient.fetch(query, {slug: params?.slug})
-    if (!post) {
-        return {
-            notFound: true
-        }
-    }
-    post = post[0]
+  let post = await sanityClient.fetch(query, {slug: params?.slug})
+  if (!post) {
     return {
-        props: {
-            post,
-        },
-        revalidate: 60 //after 60 seconds it will update content
+      notFound: true
     }
+  }
+  post = post[0]
+  return {
+    props: {
+      post
+    },
+    revalidate: 60 //after 60 seconds it will update content
+  }
 }
