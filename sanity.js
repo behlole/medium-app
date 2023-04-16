65ee23c5e4df2fd65dbcec050070fc2e89e7827f
behlole <behloleaqil@gmail.com>
2023-04-15 21:43:13 -0400

    implemented tailwind styles

--- sanity.js
+++ sanity.js
@@ -4,7 +4,7 @@ import {
 import createImageUrlBuilder from '@sanity/image-url';
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "hgt6paqh",
     apiVersion: '2021-03-25',
     useCdn: process.env.NODE_ENV === 'production'
 }
